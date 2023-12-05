09e4b93daa6dbaa6ef9444d998a59fc10ba022da
Daniel Hintz <daniel.hintz@protonmail.com>
2023-12-04 21:16:06 -0500

    day04

---
+++ day04/day04.cpp
@@ -0,0 +1,53 @@
+#include <array>
+#include <cstdint>
+#include <iostream>
+#include <numeric>
+#include <utility>
+#include <vector>
+
+constexpr auto doIt()
+{
+    constexpr std::array<std::pair<std::array<int, 10>, std::array<int, 25>>, 214> input =
+#include "input.txt" // Required formatting the input slightly
+        ;
+
+    uint64_t counts[0xffff] = {};
+    uint64_t copies[std::size(input)] = {};
+    std::fill(std::begin(copies), std::end(copies), 1);
+
+    uint64_t sum = 0;
+    for (unsigned i = 0; i < std::size(input); ++i)
+    {
+        const auto& card = input[i];
+        for (const auto winner : card.first)
+            counts[winner] = 1;
+        for (const auto mine : card.second)
+            counts[mine]++;
+        uint64_t cur = 0;
+        uint64_t new_copies = 0;
+        for (auto num : counts)
+        {
+            if (num == 2)
+            {
+                if (!cur)
+                    cur = 1;
+                else
+                    cur *= 2;
+                ++new_copies;
+            }
+        }
+        for (unsigned x = 0; x < new_copies; ++x)
+        {
+            copies[x + i + 1] += copies[i];
+        }
+        std::fill(std::begin(counts), std::end(counts), 0);
+        sum += cur;
+    }
+    return std::make_pair(sum, std::accumulate(std::begin(copies), std::end(copies), 0ull));
+}
+
+int main()
+{
+    auto [p1, p2] = doIt();
+    std::cout << "Part1: " << p1 << "\nPart2: " << p2 << std::endl;
+}
