693caa177c0414762b967dc105b8c3f6050cb233
Daniel Hintz <daniel.hintz@protonmail.com>
2023-12-04 21:22:55 -0500

    upload inputs

--- day04/day04.cpp
+++ day04/day04.cpp
@@ -1,4 +1,5 @@
 #include <array>
+#include <cmath>
 #include <cstdint>
 #include <iostream>
 #include <numeric>
@@ -10,8 +11,7 @@ constexpr auto doIt()
     constexpr std::array<std::pair<std::array<int, 10>, std::array<int, 25>>, 214> input =
 #include "input.txt" // Required formatting the input slightly
         ;
-
-    uint64_t counts[0xffff] = {};
+    uint8_t counts[0xffff] = {};
     uint64_t copies[std::size(input)] = {};
     std::fill(std::begin(copies), std::end(copies), 1);
 
@@ -23,25 +23,15 @@ constexpr auto doIt()
             counts[winner] = 1;
         for (const auto mine : card.second)
             counts[mine]++;
-        uint64_t cur = 0;
         uint64_t new_copies = 0;
         for (auto num : counts)
-        {
             if (num == 2)
-            {
-                if (!cur)
-                    cur = 1;
-                else
-                    cur *= 2;
                 ++new_copies;
-            }
-        }
+        if (new_copies)
+            sum += std::pow(2, new_copies - 1);
         for (unsigned x = 0; x < new_copies; ++x)
-        {
             copies[x + i + 1] += copies[i];
-        }
         std::fill(std::begin(counts), std::end(counts), 0);
-        sum += cur;
     }
     return std::make_pair(sum, std::accumulate(std::begin(copies), std::end(copies), 0ull));
 }
